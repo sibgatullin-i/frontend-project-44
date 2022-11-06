f96f18df5310b3a7d5944cee3f6bde9f797d0842
sibgatullin-i <ilya.sibgatullin@gmail.com>
2022-11-06 21:57:37 +0300

    5

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
 import * as cli from '../src/cli.js';
+import * as games from '../src/games.js';
 
 console.log('Welcome to Brain Games!');
-cli.userGreet();
+const name = cli.userGreet();
+games.oddEven(name);
+
+console.log('end of brain games');
