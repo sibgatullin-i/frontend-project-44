750fe2fe70da14d8c6612e48c7103ec6a733dba8
sibgatullin-i <ilya.sibgatullin@gmail.com>
2022-11-06 10:35:31 +0300

    3

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+export function userGreet() {
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}`);
+    return name;
+}
+
