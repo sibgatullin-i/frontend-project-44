66f9d65fba0c4139b9942218ca2bffed35d3855b
sibgatullin-i <67420207+sibgatullin-i@users.noreply.github.com>
2022-11-20 14:37:48 +0300

    1

--- games/brain-calc.js
+++ games/brain-calc.js
@@ -9,29 +9,18 @@ const multiplier = 10; // you may adjust it to really test yourself
 export default function brainCalc(correctAnswersRequired) {
   console.log('What is the result of the expression?');
   while (successCounter < correctAnswersRequired) {
-    const randomValue1 = Math.random() * multiplier;
-    const randomValue2 = Math.random() * multiplier;
-    const randomNum1 = Math.floor(randomValue1);
-    const randomNum2 = Math.floor(randomValue2);
-    /* const operatorRandomSeed = Math.random();
-    let randomOperator = '';
-    let correctAnswer = 0;
-    if (operatorRandomSeed >= 0.66) {
-      randomOperator = '*';
-      correctAnswer = randomNumbers[0] * randomNumbers[1];
-    } else if (operatorRandomSeed <= 0.33) {
-      randomOperator = '+';
-      correctAnswer = randomNumbers[0] + randomNumbers[1];
-    } else {
-      randomOperator = '-';
-      correctAnswer = randomNum1 - randomNum2;
-    } */
+    const randomNumbers = [];
+    for (let i = 0; i < 2; i += 1) {
+      randomNumbers.push(Math.random());
+      randomNumbers[i] *= multiplier;
+      randomNumbers[i] = Math.floor(randomNumbers[i]);  
+    }
     const randomOperator = randomItem(['+', '-', '*']);
     let correctAnswer = 0;
-    correctAnswer = randomOperator === '+' ? randomNum1 + randomNum2 : correctAnswer;
-    correctAnswer = randomOperator === '-' ? randomNum1 - randomNum2 : correctAnswer;
-    correctAnswer = randomOperator === '*' ? randomNum1 * randomNum2 : correctAnswer;
-    console.log(`Question: ${randomNum1} ${randomOperator} ${randomNum2} = ?`);
+    correctAnswer = randomOperator === '+' ? randomNumbers[0] + randomNumbers[1] : correctAnswer;
+    correctAnswer = randomOperator === '-' ? randomNumbers[0] - randomNumbers[1] : correctAnswer;
+    correctAnswer = randomOperator === '*' ? randomNumbers[0] * randomNumbers[1] : correctAnswer;
+    console.log(`Question: ${randomNumbers[0]} ${randomOperator} ${randomNumbers[1]} = ?`);
     const userAnswer = Number(readlineSync.question('Your answer: '));
     if (checkAnswer(userAnswer, correctAnswer)) {
       successCounter += 1;
