0a90d8592e4e90a53d7ffd7e8878ee41481ed0ac
sibgatullin-i <67420207+sibgatullin-i@users.noreply.github.com>
2022-11-22 17:19:42 +0300

    Update brain-even.js

--- games/brain-even.js
+++ games/brain-even.js
@@ -4,11 +4,12 @@ import readlineSync from 'readline-sync';
 import { checkAnswer } from '../src/cli.js';
 
 let successCounter = 0;
+const multiplier = 1000; // adjust this to really test yourself
 
 export default function brainEven(correctAnswersRequired) {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   while (successCounter < correctAnswersRequired) {
-    const randomNum = Math.floor(Math.random() * 1000);
+    const randomNum = Math.floor(Math.random() * multiplier);
     console.log(`Question: ${randomNum}`);
     const userAnswer = readlineSync.question('Your answer: ');
     const correctAnswer = randomNum % 2 === 0 ? 'yes' : 'no';
